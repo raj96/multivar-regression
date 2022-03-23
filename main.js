94e1f5ddf002e051d9066d19b593a82b905e85b7
Raja Nand Sharma <rajpeaks@gmail.com>
2022-03-23 19:46:55 +0530

    Lowering learning rate and increasing epochs per draw loop

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 const rangeMin = 0;
 const rangeMax = 1;
-const alpha = 0.25;
+const alpha = 0.1;
 
 let xs = [];
 let ys = [];
@@ -10,7 +10,7 @@ let numberOfThetas = 2;
 let lineResolution = 0.1;
 let thetas = [];
 let fps;
-let correctThetasFor = 50;
+let correctThetasFor = 100;
 
 //y = theta1 * x + theta0
 function hTheta(x) {
